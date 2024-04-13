0e79210c53d03d8d83a9420a0908f53fdc2383e0
PMerka <86120382+PMerka@users.noreply.github.com>
2024-04-13 22:42:54 +0200

    prices

--- src/components/PriceList/PriceList.tsx
+++ src/components/PriceList/PriceList.tsx
@@ -2,27 +2,36 @@ import React from "react";
 import styles from "./PriceList.module.css";
 import { attributes } from "/content/priceList.md";
 
+type MdDataPriceList = {
+  title: string;
+  priceTitle: string;
+  prices: {
+    item: string;
+    price: string;
+  }[];
+  paragraphs: {
+    title: string;
+    text: string;
+  }[];
+};
+
 export default function PriceList() {
-  const { paragraphs } = attributes as {
-    paragraphs: {
-      title: string;
-      text: string;
-    }[];
-  };
+  const { title, priceTitle, prices, paragraphs } =
+    attributes as MdDataPriceList;
   return (
     <div className={`${styles.priceList} sectionsShared`} id="cenik">
-      <h2>Cena za copywriting</h2>
+      <h2>{title}</h2>
 
       <div className={styles.priceListContent}>
         <div className={`${styles.article} ${styles.price}`}>
-          <h3>Hodinová sazba 500 Kč/hod</h3>
+          <h3>{priceTitle}</h3>
           <div className={styles.pricesExample}>
-            <div>Blogový článek (standard, 500 slov) </div>
-            <div>od 1500 Kč</div>
-            <div>Produktový popisek (80 slov) </div>
-            <div>od 120 Kč</div>
-            <div>Textace webu </div>
-            <div>od 7000 Kč</div>
+            {prices.map((row) => (
+              <>
+                <div>{row.item}</div>
+                <div>{row.price}</div>
+              </>
+            ))}
           </div>
         </div>
 
