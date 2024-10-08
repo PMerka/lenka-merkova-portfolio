cb3a46d6b0805eb4ae3adf3a93d421a4ccdba48c
PMerka <pavelmerka1995@gmail.com>
2024-10-08 18:50:30 +0200

    [UPDATE] styling

--- src/components/Services/ServiceItem/ServiceItem.tsx
+++ src/components/Services/ServiceItem/ServiceItem.tsx
@@ -16,9 +16,8 @@ export default function ServiceItem({
         <h3 className={styles.title}>{title}</h3>
       </div>
       <div className={styles.body}>
-        <div className={styles.shortText}>{annotation}</div>
-
         <div className={styles.mainText}>{mainText}</div>
+        <div className={styles.shortText}>{annotation}</div>
       </div>
     </div>
   );
