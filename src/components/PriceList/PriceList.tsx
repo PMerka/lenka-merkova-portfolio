10f5d97f6b58baec656053f873e24904a5e600b4
PMerka <pavelmerka1995@gmail.com>
2024-10-20 15:06:38 +0200

    [UPDATE] price layout change change

--- src/components/PriceList/PriceList.tsx
+++ src/components/PriceList/PriceList.tsx
@@ -1,46 +1,35 @@
 import React from "react";
 import styles from "./PriceList.module.css";
-import { attributes } from "/content/priceList.md";
+import priceList from "/content/priceList.json";
 
-type MdDataPriceList = {
-  title: string;
-  priceTitle: string;
-  prices: {
-    item: string;
-    price: string;
-  }[];
-  paragraphs: {
+
+type Paragraph = {
     title: string;
     text: string;
-  }[];
 };
 
 export default function PriceList() {
-  const { title, priceTitle, prices, paragraphs } =
-    attributes as MdDataPriceList;
+
   return (
     <div className={`${styles.priceList} sectionsShared`} id="cenik">
-      <h2>{title}</h2>
+      <h2>{priceList.title}</h2>
 
       <div className={styles.priceListContent}>
-        <div className={`${styles.article} ${styles.price}`}>
-          <h3>{priceTitle}</h3>
-          <div className={styles.pricesExample}>
-            {prices.map((row) => (
-              <>
-                <div>{row.item}</div>
-                <div>{row.price}</div>
-              </>
-            ))}
-          </div>
+        <div className={`${styles.price}`}>
+          <h3>{priceList.priceBox.priceLabel}</h3>
+          <h3>{priceList.priceBox.price}</h3>
         </div>
 
-        {paragraphs.map((paragraph, index) => (
+        <div>
+        {priceList.paragraphs.map((paragraph: Paragraph, index: number) => (
           <div key={index} className={styles.article}>
-            <h3>{paragraph.title}</h3>
-            <p>{paragraph.text}</p>
+            <h3>{paragraph?.title}</h3>
+            <p>{paragraph?.text}</p>
           </div>
         ))}
+        </div>
+
+
       </div>
     </div>
   );
