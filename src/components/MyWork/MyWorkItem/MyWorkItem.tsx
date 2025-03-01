d7c2a6388cd8d0b6e118abe8253b2e40304c749d
PMerka <pavelmerka1995@gmail.com>
2025-03-01 17:41:53 +0100

    [UPDATE]

--- src/components/MyWork/MyWorkItem/MyWorkItem.tsx
+++ src/components/MyWork/MyWorkItem/MyWorkItem.tsx
@@ -4,20 +4,20 @@ import styles from "./MyWorkItem.module.css";
 export default function MyWorkItem({
   title,
   imgSrc,
-  link
+  link,
 }: {
   title?: string;
   imgSrc?: string;
-  link?: string
+  link?: string;
 }) {
   return (
     <a href={link} target="_blank">
-    <div className={styles.myWorkItem}>
-      <div className={styles.titleSection}>
-        <h4 className={styles.title}>{title}</h4>
+      <div className={styles.myWorkItem}>
+        <img src={imgSrc} alt="" className={styles.myWorkItemImg} />{" "}
+        <div className={styles.titleSection}>
+          <h4 className={styles.title}>{title}</h4>
+        </div>
       </div>
-      <img src={imgSrc} alt="" className={styles.myWorkItemImg} />
-    </div>
     </a>
   );
 }
