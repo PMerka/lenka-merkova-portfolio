96361585c9d3b0ef211280f5df0c31b929a6da9b
PMerka <pavelmerka1995@gmail.com>
2025-03-01 17:30:00 +0100

    [ADD] labels

--- src/components/MyWork/MyWork.tsx
+++ src/components/MyWork/MyWork.tsx
@@ -6,7 +6,7 @@ import { html, attributes } from "/content/myWork.md";
 export default function MyWork() {
   const { title, myWorkItem } = attributes as {
     title: string;
-    myWorkItem: { name: string; picture: string; link: string }[];
+    myWorkItem: { topText: string; picture: string; link: string }[];
   };
 
   return (
@@ -14,7 +14,12 @@ export default function MyWork() {
       <h2>{title}</h2>
       <div className={styles.myWorkGrid}>
         {myWorkItem.map((item, index) => (
-          <MyWorkItem key={index} title={item.name} imgSrc={item.picture} link={item.link}/>
+          <MyWorkItem
+            key={index}
+            title={item.topText}
+            imgSrc={item.picture}
+            link={item.link}
+          />
         ))}
       </div>
     </div>
